a304e81c5afbfe944b923ede19dee58f8c3c11c7
duy <www.duynguyen2k3@gmail.com>
2023-08-27 14:43:21 +0700

    bai 1

---
+++ bai1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhập tên tọa độ: ");
+            string name = Console.ReadLine();
+            a.SetName(name);
+            toado a = new toado();
+            Console.Write("Nhập giá trị x: ");
+            int x = int.Parse(Console.ReadLine());
+            a.SetX(x);
+            Console.Write("Nhập giá trị y: ");
+            int y = int.Parse(Console.ReadLine());
+            a.SetY(y);
+            Console.WriteLine(a.ToString());
+        }
+    }
+}
---
+++ bai1/toado.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai1
+{
+    public class toado
+
+    {
+        private int x;
+        private int y;
+        private string name;
+
+        public toado()
+        {
+            x = 0;
+            y = 0;
+            name = "";
+        }
+        public toado(int x, int y, string name)
+        {
+            this.x = x;
+            this.y = y;
+            this.name = name;
+        }
+        public void SetX(int x)
+        {
+            this.x = x;
+        }
+
+        public void SetY(int y)
+        {
+            this.y = y;
+        }
+
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+        public int GetX()
+        {
+            return x;
+        }
+
+        public int GetY()
+        {
+            return y;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+        public override string ToString()
+        {
+            return name + " (" + x + ", " + y + ")";
+        }
+    }
+}
